1ac49816d66a94c05f96b5a2ee613cd3c9a8fb29
Gabriel Bucalon <gabriel.bucalon@synvia.com.br>
2019-10-24 22:02:31 -0300

    <feat>: create page home page

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,47 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Administrador
  */
 public class Main extends Application {
-    
+       //define your offsets here
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/FXMLDocument.fxml"));
-        
+         Parent root = FXMLLoader.load(getClass().getResource("view/FXMLDocument.fxml"));
+
+        //you can use underdecorated or transparent.
+        stage.initStyle(StageStyle.TRANSPARENT);
+        //stage.initStyle(StageStyle.UNDERDECORATED);
+
+        //grab your root here
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        //move around here
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+
         stage.show();
     }
 
--- src/controller/FXMLDocumentController.java
+++ src/controller/FXMLDocumentController.java
@@ -3,33 +3,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package anivation;
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import static javafx.scene.paint.Color.BLUE;
+import static javafx.scene.paint.Color.ORANGE;
+import static javafx.scene.paint.Color.PURPLE;
 
 /**
  *
  * @author Administrador
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Label lblAnimes;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Pane paneSearchAnimes;
+    @FXML
+    private Pane panelBestAnimes;
+    @FXML
+    private Pane panelAllAnimes;
+    @FXML
+    private Pane searchAnime;
+    @FXML
+    private Button btnAllAnimes;
+    @FXML
+    private Button btnSearchAnime;
+    @FXML
+    private Button btnBestAnimes;
+    @FXML
+    private TextField txtSearchAnime;
+    @FXML
+    private Button btnFetchAnime;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void actionBtnAllAnimes(ActionEvent event) {
+        lblAnimes.setText("Todos Animes");
+        lblAnimes.setTextFill(BLUE);
+        panelAllAnimes.setStyle("-fx-background-color: #6572bc;");
+        paneSearchAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        panelBestAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        btnFetchAnime.setVisible(false);
+        txtSearchAnime.setVisible(false);
+    }
+
+    @FXML
+    private void actionBestAnimes() {
+        lblAnimes.setText("Melhores Animes");
+        lblAnimes.setTextFill(ORANGE);
+        panelAllAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        paneSearchAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        panelBestAnimes.setStyle("-fx-background-color: #ffa500;");
+        btnFetchAnime.setVisible(false);
+        txtSearchAnime.setVisible(false);
+    }
+
+    @FXML
+    private void actionSearchAnimes() {
+        lblAnimes.setText("Buscar Anime");
+        lblAnimes.setTextFill(PURPLE);
+        panelAllAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        paneSearchAnimes.setStyle("-fx-background-color: #ad75ad;");
+        panelBestAnimes.setStyle("-fx-background-color: #d3d3d3;");
+        btnFetchAnime.setVisible(true);
+        txtSearchAnime.setVisible(true);
+    }
+
+    @FXML
+    private void actionSearchAnime() {
+        
+    }
+
 }
