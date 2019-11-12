89b55bb190d43724fa8b030e188ef1cc92e085e1
Gabriel Bucalon <gabriel.bucalon@synvia.com.br>
2019-11-12 20:49:59 -0300

    <feat>: back to page

--- src/controller/FXML_addSeriesController.java
+++ src/controller/FXML_addSeriesController.java
@@ -7,10 +7,14 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -29,24 +33,34 @@ public class FXML_addSeriesController implements Initializable {
     private TextArea txtAnimeSynopsis;
     @FXML
     private TextArea txtAnimeComment;
+
+    Stage dialogStage = new Stage();
+    Scene scene;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
+
     @FXML
-    private void createAnime(){
-        
+    private void createAnime() {
+
     }
-    
+
     @FXML
-    private void backToPage(){
-            
+    private void backToPage(ActionEvent event) {
+        Node source = (Node) event.getSource(); // Pega o evento do botão
+        dialogStage = (Stage) source.getScene().getWindow();
+        dialogStage.close();
     }
-    
+
     @FXML
-    private void clearFields(){
-       
+    private void clearFields() {
+        txtAnimeComment.clear();
+        txtAnimeEpisodes.clear();
+        txtAnimeName.clear();
+        txtAnimeSynopsis.clear();
+        txtSearchtxtAnimeGenre.clear();
     }
 
 }
